5a8a83ff1b101bb1baabf9b6abc51863c6ece7be
MarlineKhavele <marlinekhavele6@gmail.com>
2021-02-22 00:54:49 +0300

    Added comment serializer

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from blog.models import Post
+from blog.models import Post, Comment
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['title','body','content','updated']
+
+class Comment(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['name','email','body','updated']
